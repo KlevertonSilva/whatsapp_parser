d8d175b397df912dbeee97d53079d06b5058319d
PepeZuco <joaozuco@gmail.com>
2024-05-24 13:49:52 -0300

    criptografia

--- utils.py
+++ utils.py
@@ -5,20 +5,34 @@ from cryptography.fernet import Fernet
 
 
 class Utils:
+    _key = None
+    _cipher_suite = None
 
-    key = Fernet.generate_key()
-    cipher_suite = Fernet(key)
+    @staticmethod
+    def generate_and_store_key():
+        """Generate a key for encryption and store it securely."""
+        Utils._key = Fernet.generate_key()
+        Utils._cipher_suite = Fernet(Utils._key)
+        # Store the key securely (e.g., in an environment variable or secure storage)
+        with open('secret.key', 'wb') as key_file:
+            key_file.write(Utils._key)
 
+    @staticmethod
+    def load_key():
+        """Load the key from secure storage."""
+        if Utils._key is None:
+            with open('secret.key', 'rb') as key_file:
+                Utils._key = key_file.read()
+            Utils._cipher_suite = Fernet(Utils._key)
+
+    @staticmethod
     def read_language_files(language: str):
         file_path = f'language/{language}.json'
         with open(file_path, 'r') as file:
             return json.load(file)
 
-    def check_and_apply_filter_dates(start_date,
-                                     end_date,
-                                     dataframe) -> pd.DataFrame:
-
-        # Filter chat DataFrame based on date range
+    @staticmethod
+    def check_and_apply_filter_dates(start_date, end_date, dataframe) -> pd.DataFrame:
         if start_date and end_date:
             start_date = datetime.strptime(start_date, '%Y-%m-%d').date()
             end_date = datetime.strptime(end_date, '%Y-%m-%d').date()
@@ -33,16 +47,20 @@ class Utils:
             (dataframe['date'] >= start_date) & (dataframe['date'] <= end_date)
         ]
 
+    @staticmethod
     def encrypt_file(file_path):
+        Utils.load_key()
         with open(file_path, 'rb') as file:
             file_data = file.read()
-        encrypted_data = Utils.cipher_suite.encrypt(file_data)
+        encrypted_data = Utils._cipher_suite.encrypt(file_data)
         with open(file_path, 'wb') as file:
             file.write(encrypted_data)
 
+    @staticmethod
     def decrypt_file(file_path):
+        Utils.load_key()
         with open(file_path, 'rb') as file:
             encrypted_data = file.read()
-        decrypted_data = Utils.cipher_suite.decrypt(encrypted_data)
+        decrypted_data = Utils._cipher_suite.decrypt(encrypted_data)
         with open(file_path, 'wb') as file:
             file.write(decrypted_data)
